614a0e40114d49debc186cbab5873a1cdbf9c9e4
dimitrilzk <dimitrilazuka95@gmail.com>
2022-11-22 14:31:28 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w1_l1_Intro_OOP_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
