56c90562cb1b524393e6dc84e424b57ede3b3b25
dimitrilzk <dimitrilazuka95@gmail.com>
2022-11-23 21:42:53 +0100

    finito

---
+++ CV.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w1_l1_Intro_OOP_1
+{
+    internal class CV
+    {
+        public InformazioniPersonali InfoPersonali { get; set; }
+        public List<Studi> StudiEffetuati { get; set; }
+        public List<Impiego> Impieghi { get; set; }
+        public void StampaCvCandidato(CV cv1)
+        {
+            Console.WriteLine($"CV di {cv1.InfoPersonali.Cognome} {cv1.InfoPersonali.Nome}");
+            Console.WriteLine("");
+            Console.WriteLine("++++ INIZIO Informazioni Personali: ++++");
+            Console.WriteLine($"Nome: {cv1.InfoPersonali.Nome}");
+            Console.WriteLine($"Cognome: {cv1.InfoPersonali.Cognome}");
+            Console.WriteLine($"Email: {cv1.InfoPersonali.Email}");
+            Console.WriteLine($"Telefono: {cv1.InfoPersonali.Telefono}");
+            Console.WriteLine("++++ FINE Informazioni Personali: ++++");
+            Console.WriteLine("");
+
+            Console.WriteLine("++++ INIZIO Studi e Formazione: ++++");
+            foreach (Studi studio in cv1.StudiEffetuati)
+            {
+                Console.WriteLine($"Istituto: {studio.Istituto}");
+                Console.WriteLine($"Qualifica: {studio.Qualifica}");
+                Console.WriteLine($"Tipo: {studio.Tipo}");
+                Console.WriteLine($"Dal {studio.Dal} al {studio.Al}");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("++++ FINE Studi e Formazione: ++++");
+            Console.WriteLine("");
+
+            Console.WriteLine("++++ INIZIO Esperienze professionali: ++++");
+            foreach (Impiego impiego in cv1.Impieghi)
+            {
+                Console.WriteLine($"Presso: {impiego.EsperienzeLavorative.Azienda}");
+                Console.WriteLine($"Tipo di lavoro: {impiego.EsperienzeLavorative.JobTitle}");
+                Console.WriteLine($"Compito: {impiego.EsperienzeLavorative.Compiti}");
+                Console.WriteLine($"Dal: {impiego.EsperienzeLavorative.Dal} al {impiego.EsperienzeLavorative.Al}");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("++++ FINE Esperienze professionali: ++++");
+            Console.WriteLine("");
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+    internal class InformazioniPersonali
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+
+    }
+    internal class Studi
+    {
+        public string Qualifica { get; set; }
+        public string Istituto { get; set; }
+        public string Tipo { get; set; }
+        public DateTime Dal { get; set; }
+        public DateTime Al { get; set; }
+    }
+    internal class Esperienza
+    {
+        public string Azienda { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime Dal { get; set; }
+        public DateTime Al { get; set; }
+        public string Descrizione { get; set; }
+        public string Compiti { get; set; }
+
+    }
+    internal class Impiego
+    {
+        public Esperienza EsperienzeLavorative;
+    }
+}
+
+    
--- Program.cs
+++ Program.cs
@@ -10,6 +10,67 @@ namespace w1_l1_Intro_OOP_1
     {
         static void Main(string[] args)
         {
+            CV cv1 = new CV();
+            InformazioniPersonali InfoPersonali = new InformazioniPersonali()
+            {
+                Nome = "Mario",
+                Cognome = "Rossi",
+                Telefono = "3471536456",
+                Email = "example@ex.com"
+            };
+            cv1.InfoPersonali = InfoPersonali;
+
+            List<Studi> ListaDegliStudi = new List<Studi>();
+            Studi Universita = new Studi()
+            {
+                Istituto = "Bocconi",
+                Qualifica = "Sviluppatore",
+                Dal = new DateTime(2015, 09, 01),
+                Al = new DateTime(2020, 06, 05),
+            };
+            Studi Corso = new Studi()
+            {
+                Istituto = "Epicode School",
+                Qualifica = "Full-stack Web Developer",
+                Dal = new DateTime(2022, 09, 30),
+                Al = new DateTime(2023, 02, 15),
+            };
+            ListaDegliStudi.Add(Universita);
+            ListaDegliStudi.Add(Corso);
+            cv1.StudiEffetuati = ListaDegliStudi;
+
+            List<Impiego> EsperienzeLavorative = new List<Impiego>();
+            Impiego Commerciale = new Impiego()
+            {
+                EsperienzeLavorative = new Esperienza()
+                {
+                    Azienda = "ABTG",
+                    JobTitle = "Sales Manager",
+                    Dal = new DateTime(2020, 02, 15),
+                    Al = new DateTime(2021, 08, 10),
+                    Descrizione = "Vendita corsi di formazione",
+                    Compiti = "Raggiungimento di obiettivi minimi di fatturato annuo",
+                }
+            };
+            Impiego Developer = new Impiego()
+            {
+                EsperienzeLavorative = new Esperienza()
+                {
+                    Azienda = "Microsoft",
+                    JobTitle = "Back-end Developer",
+                    Dal = new DateTime(2022, 02, 15),
+                    Al = new DateTime(2025, 08, 10),
+                    Descrizione = "Programmazione di oggetti",
+                    Compiti = "Far andare avanti la Microsoft",
+                }
+            };
+            EsperienzeLavorative.Add(Commerciale);
+            EsperienzeLavorative.Add(Developer);
+            cv1.Impieghi = EsperienzeLavorative;
+            cv1.StampaCvCandidato(cv1);
+
+
+
         }
     }
 }
